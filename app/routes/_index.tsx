a5305cae0825c872ec776f536d00f1c7dc31ab74
leochiu <leochiu@hahow.in>
2024-11-20 11:31:05 +0800

    feat: add input component

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
 import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 
 export const meta: MetaFunction = () => {
   return [
@@ -11,7 +12,17 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div>
-      <Button>Click me</Button>
+      <Card>
+        <CardHeader>
+          <CardTitle>My First Github Commit</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Input placeholder="Your GitHub name" v-model="username" />
+        </CardContent>
+        <CardFooter>
+          <Button onClick="emit('search', username)">Search</Button>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
